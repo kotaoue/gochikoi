3f9ea6d74f764c193d1baeac5dd401d22b6809d7
kotaoue <kotaoue@gmail.com>
2026-03-05 00:17:12 +0900

    feat: 変換のダミーを追加

--- main.go
+++ main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+
+	"github.com/kotaoue/gochikoi/pkg/gachikoi"
 )
 
 func main() {
-	for _, arg := range os.Args[1:] {
-		fmt.Println(arg)
+	args := os.Args[1:]
+	if len(args) > 0 {
+		for _, arg := range args {
+			fmt.Println(gachikoi.Translate(arg))
+		}
+		return
 	}
+
+	in, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if len(in) == 0 {
+		return
+	}
+
+	fmt.Print(gachikoi.Translate(string(in)))
 }
